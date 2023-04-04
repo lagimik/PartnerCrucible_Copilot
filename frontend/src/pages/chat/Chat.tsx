affdc9e7caf28512b2dc5e57210ed84fc28b62aa
Marc-Andre Morisset <mamoris@microsoft.com>
2023-04-04 16:30:24 -0400

    reverted back to sparkles

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -134,7 +134,7 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <img src="../../assets/partnercrucible.jpg" aria-label="Partner Crucible Logo"/>
+                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
                             <h1 className={styles.chatEmptyStateTitle}>Chat with The Partner Crucible</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
